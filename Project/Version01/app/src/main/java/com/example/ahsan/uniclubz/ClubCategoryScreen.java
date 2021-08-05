8423f766c5257dc8146b17eb487068a0d075d68b
Ahsan Saad <71340161+Ahsan-Saad@users.noreply.github.com>
2021-08-05 22:35:04 +0600

    Adding Club Category Screen

---
+++ Project/Version01/app/src/main/java/com/example/ahsan/uniclubz/ClubCategoryScreen.java
@@ -0,0 +1,14 @@
+package com.example.ahsan.uniclubz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClubCategoryScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.club_category_screen);
+    }
+}
