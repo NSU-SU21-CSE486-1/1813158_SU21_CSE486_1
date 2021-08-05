0ccd2f8369535ec90660831b1692316933b783fc
Ahsan Saad <71340161+Ahsan-Saad@users.noreply.github.com>
2021-08-05 20:25:52 +0600

    Adding General User's Dashboard Screen

---
+++ Project/Version01/app/src/main/java/com/example/ahsan/uniclubz/GeneralUsersDash.java
@@ -0,0 +1,14 @@
+package com.example.ahsan.uniclubz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneralUsersDash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.general_users_dash);
+    }
+}
---
+++ Project/Version01/app/src/main/java/com/example/ahsan/uniclubz/WelcomeScreen.java
@@ -0,0 +1,21 @@
+package com.example.ahsan.uniclubz;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class WelcomeScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+    }
+}
