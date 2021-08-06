19e1ec1c97cefa119bd00474a1a4dd9bd8d534e3
Ahsan Saad <71340161+Ahsan-Saad@users.noreply.github.com>
2021-08-06 18:05:50 +0600

    Adding Club Join Invitations Screen

---
+++ Project/Version01/app/src/main/java/com/example/ahsan/uniclubz/JoinInvitationScreen.java
@@ -0,0 +1,14 @@
+package com.example.ahsan.uniclubz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinInvitationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_invitation_screen);
+    }
+}
