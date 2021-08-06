9792a0c6382a21a90fa2769d2cf3632efa9ecbe4
Ahsan Saad <71340161+Ahsan-Saad@users.noreply.github.com>
2021-08-06 17:40:04 +0600

    Adding Events Screen

---
+++ Project/Version01/app/src/main/java/com/example/ahsan/uniclubz/EventsScreen.java
@@ -0,0 +1,14 @@
+package com.example.ahsan.uniclubz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events_screen);
+    }
+}
