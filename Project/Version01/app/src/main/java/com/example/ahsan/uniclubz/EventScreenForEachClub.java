e4e6164e02fc69f2aba9e7548da9091944ead01d
Ahsan Saad <71340161+Ahsan-Saad@users.noreply.github.com>
2021-08-14 00:00:47 +0600

    Adding Events Screen for each clubs

---
+++ Project/Version01/app/src/main/java/com/example/ahsan/uniclubz/EventScreenForEachClub.java
@@ -0,0 +1,14 @@
+package com.example.ahsan.uniclubz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventScreenForEachClub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_screen_for_each_club);
+    }
+}
