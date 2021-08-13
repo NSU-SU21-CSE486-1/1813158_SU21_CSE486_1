5faebc70881e0b09a1c6cf997feca19ca3afa673
Ahsan Saad <71340161+Ahsan-Saad@users.noreply.github.com>
2021-08-13 23:54:22 +0600

    Adding Club List Screens

---
+++ Project/Version01/app/src/main/java/com/example/ahsan/uniclubz/ClubListScreen_CSE.java
@@ -0,0 +1,14 @@
+package com.example.ahsan.uniclubz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClubListScreen_CSE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.club_list_screen_cse);
+    }
+}
